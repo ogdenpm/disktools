cb852d99b02356807a44a4b85bae5212f1795e15
Mark Ogden <mark.pm.ogden@btinternet.com>
2024-06-06 14:44:26 +0100

    Updated verison tools

---
+++ shared/appinfo.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+ *  appinfo.h: part of the C port of Intel's ISIS-II ixref             *
+ *  The original ISIS-II application is Copyright Intel                     *
+ *                                                                          *
+ *  Re-engineered to C by Mark Ogden <mark.pm.ogden@btinternet.com>         *
+ *                                                                          *
+ *  It is released for academic interest and personal use only              *
+ ****************************************************************************/
+
+// static version information
+// overrides are in _appinfo.h in the source directory
+#ifndef _APPINFO_H_
+#define _APPINFO_H_
+#include <_appinfo.h>
+#ifndef APP_PRODUCT
+#define APP_PRODUCT APP_NAME
+#endif
+#ifndef APP_OWNER
+#define APP_OWNER "Mark Ogden"
+#endif
+#define APP_EMAIL "support@mark-ogden.uk"
+#endif
