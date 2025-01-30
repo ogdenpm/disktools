3bc8884ff8575d18d0a73191c4a539cf6177301d
Mark Ogden <mark.pm.ogden@btinternet.com>
2025-01-30 14:17:07 +0000

    bin2imd - 2025.1.30.1: Cleaned build for Linux and moved to common utilities

---
+++ bin2imd/_appinfo.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "imdVersion.h"
+#define APP_NAME        "bin2imd"
+#define APP_PRODUCT     "bin2imd 1.20"
+#define APP_DESCRIPTION "Convert binary file to IMD"
+#define APP_OWNER       "Dave Dunfield"
+#define APP_MODS        "Port - Mark Ogden"
